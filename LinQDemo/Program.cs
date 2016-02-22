e4b9cbb8f969bc300b935b8374be852f75a3b7a2
Cesar Lopez <cesarl@outlook.com>
2016-02-22 11:58:13 -0700

    Deferred execution1 and 2

--- LinQDemo/Program.cs
+++ LinQDemo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.CodeDom;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -24,32 +26,98 @@ namespace LinQDemo
 
         public void Menu()
         {
-            Console.WriteLine("A: NumbersGreaterThan5");
-            Console.WriteLine("B: MultipleReturnValues1");
-            Console.WriteLine("C: MultipleReturnValues2");
-
-            Console.WriteLine("\nEnter an Option (Press . to exit):");
-            
-            var key = Char.ToUpper(Console.ReadKey(true).KeyChar);
-            Console.WriteLine(key);
-            Thread.Sleep(200);
-            
-            Console.Clear();
-
-            switch (key)
+            while (true)
             {
-                case 'A':
-                    NumbersGreatherThan5();
-                    break;
-                case 'B':
-                    MultipleReturnValues1();
-                    break;
-                case 'C':
-                    MultipleReturnValues2();
-                    break;
-            }
+                Console.Clear();
 
-            Console.ReadKey(true);
+                Console.WriteLine("A: NumbersGreaterThan5");
+                Console.WriteLine("B: MultipleReturnValues1");
+                Console.WriteLine("C: MultipleReturnValues2");
+                Console.WriteLine("D: DeferredExecution1");
+                Console.WriteLine("E: DeferredExecution2");
+
+                Console.WriteLine("\nEnter an Option (Press . to exit):");
+
+                var key = Char.ToUpper(Console.ReadKey(true).KeyChar);
+                Console.WriteLine(key);
+                Thread.Sleep(200);
+
+                Console.Clear();
+
+                switch (key)
+                {
+                    case 'A':
+                        NumbersGreatherThan5();
+                        break;
+                    case 'B':
+                        MultipleReturnValues1();
+                        break;
+                    case 'C':
+                        MultipleReturnValues2();
+                        break;
+                    case 'D':
+                        DeferredExecution1();
+                        break;
+                    case 'E':
+                        DeferredExecution2();
+                        break;
+                    default:
+                        return;
+                }
+
+                Console.ReadKey(true);
+
+            }
+            
+        }
+
+        private void DeferredExecution2()
+        {
+            int[] numbers = { 1, 5, 3, 6, 10, 12, 8 };
+
+            var query = from number in numbers
+                orderby number descending
+                select number;
+
+            var query1 = from number in query
+                where number < 8
+                select number;
+
+            var query2 = from number in query1
+                select DoubleIt(number);
+
+            DisplayResults("Deferred Execution revisited", query2);
+        }
+
+
+        private void DeferredExecution1()
+        {
+            int[] numbers = {1,5,3,6,10,12,8};
+
+            var query = from number in numbers
+                select DoubleIt(number);
+
+            DisplayResults("Deferred Execution", query);
+        }
+        private long DoubleIt(int value)
+        {
+            DisplayResults("About to double the number " + value);
+            return value*2;
+        }
+
+        private void DisplayResults(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        private void DisplayResults(string msg, IEnumerable items)
+        {
+            Console.WriteLine(msg + "\n============================================");
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
         }
 
         private void NumbersGreatherThan5()
