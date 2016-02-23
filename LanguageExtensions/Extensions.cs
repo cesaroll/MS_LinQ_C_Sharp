54381b95e6fbcf0797816dc624d915dac38ec7f7
Cesar Lopez <cesarl@outlook.com>
2016-02-23 11:34:08 -0700

    Lambda parameters, Extension Methods, Query Methods, Enumerable Methods, Anonymous type

---
+++ LanguageExtensions/Extensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LanguageExtensions;
+
+namespace LinQDemo
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Retuns true if odd number
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsOdd(this int value)
+        {
+            return value%2 != 0;
+        }
+
+        /// <summary>
+        /// Returns true if even number
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsEven(this int value)
+        {
+            return !IsOdd(value);
+        }
+
+        public static long TotalSize(this IEnumerable<MyFileInfo> files)
+        {
+            long sum = 0;
+            foreach (var mfi in files)
+            {
+                sum += mfi.Length;
+            }
+
+            return sum;
+        }
+
+    }
+}
--- LanguageExtensions/Program.cs
+++ LanguageExtensions/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using LinQDemo;
 
 namespace LanguageExtensions
 {
@@ -23,6 +26,170 @@ namespace LanguageExtensions
             prog.Menu();
 
         }
+
+        #region Anonymous Type
+
+        private void AnonymousType()
+        {
+            DisplayResults("Anonymous Type");
+            SearchForFiles9(_searchPath, fi => fi.Length > 1024);
+        }
+
+        private void SearchForFiles9(string path, Predicate<FileInfo> condition)
+        {
+            var items = new DirectoryInfo(path).GetFiles();
+            var files = from item in items
+                where condition(item)
+                orderby item.Length descending
+                select new
+                {
+                    Name = item.Name,
+                    Size = item.Length,
+                    CreationTime = item.CreationTime
+                };
+
+            
+
+            //Notice you do not have the override ToString on MyFileInfo
+            //since we are not using MyFileInfo class
+            DisplayResults("Selected Files in anonymous type:", files);
+
+
+            //Make one collection for display only
+            var display = from item in files
+                select String.Format("{0} {1,15:N0}  {2}",
+                    item.CreationTime.ToString("g", DateTimeFormatInfo.InvariantInfo),
+                    item.Size,
+                    item.Name);
+
+            DisplayResults("\nMerge anonymous type for better display:", display);
+
+        }
+
+        #endregion
+
+        #region Enumerable methods
+        /// <summary>
+        /// Enumerable methods
+        /// More COmplicated than Query Methods
+        /// Do Not use ...
+        /// </summary>
+        private void EnumerableMethods()
+        {
+            DisplayResults("Enumerable Methods");
+            SearchForFiles8a(_searchPath, fi => fi.Length > 512, mfi => -mfi.Length);
+        }
+
+        private void SearchForFiles8a(string path, Predicate<FileInfo> condition, Func<MyFileInfo, long> sorting)
+        {
+            var files = new List<MyFileInfo>();
+
+            foreach (var fi in new DirectoryInfo(path).GetFiles())
+            {
+                if (condition(fi))
+                {
+                    files.Add(new MyFileInfo() { Name = fi.Name, Length = fi.Length, CreationTime = fi.CreationTime });
+                }
+            }
+
+            DisplayResults("Filter using function parameter: " + path, files.OrderBy(sorting));
+
+            DisplayResults("\nTotal Size: " + files.TotalSize().ToString("N0"));
+
+            DisplayResults("\nTop 5 Total Size: " + 
+                Enumerable.Sum(
+                    Enumerable.Take(
+                        Enumerable.OrderByDescending(files, mfi => mfi.Length), 5), 
+                mfi => mfi.Length).ToString("N0"));
+
+        }
+
+        #endregion
+
+        #region Query methods
+
+        private void QeryMethods()
+        {
+            DisplayResults("Query methods:");
+            SearchForFiles8(_searchPath, fi => fi.Length > 512, mfi => -mfi.Length);
+        }
+        private void SearchForFiles8(string path, Predicate<FileInfo> condition, Func<MyFileInfo, long> sorting)
+        {
+            var files = new List<MyFileInfo>();
+
+            foreach (var fi in new DirectoryInfo(path).GetFiles())
+            {
+                if (condition(fi))
+                {
+                    files.Add(new MyFileInfo() { Name = fi.Name, Length = fi.Length, CreationTime = fi.CreationTime });
+                }
+            }
+
+            DisplayResults("Filter using function parameter: " + path, files.OrderBy(sorting));
+
+            DisplayResults("\nTotal Size: " + files.TotalSize().ToString("N0"));
+
+            DisplayResults("\nTop 5 Total Size: " + files.OrderByDescending(mfi => mfi.Length).Take(5).Sum(mfi => mfi.Length).ToString("N0") );
+
+        }
+        #endregion
+
+        #region Extension Method
+
+        private void ExtensionMethod()
+        {
+            DisplayResults("Extension Method:");
+            SearchForFiles7(_searchPath,
+                fi => fi.Length > 512,
+                sfi => -sfi.Length);
+        }
+
+        private void SearchForFiles7(string path, Predicate<FileInfo> condition, Func<MyFileInfo, long> sorting)
+        {
+            var files = new List<MyFileInfo>();
+
+            foreach (var fi in new DirectoryInfo(path).GetFiles())
+            {
+                if (condition(fi))
+                {
+                    files.Add(new MyFileInfo() { Name = fi.Name, Length = fi.Length, CreationTime = fi.CreationTime });
+                }
+            }
+
+            DisplayResults("Filter using function parameter: " + path, files.OrderBy(sorting));
+
+            DisplayResults("\nTotal Size: " + files.TotalSize().ToString("N0"));
+
+        }
+
+        #endregion
+
+        #region Lambda parameters for sorting
+
+        private void LambdaParametersForSorting()
+        {
+            DisplayResults("Lambda parameters for sorting:");
+            SearchForFiles6c(_searchPath, 
+                fi => fi.Length > 512,
+                sfi => -sfi.Length);
+        }
+
+        private void SearchForFiles6c(string path, Predicate<FileInfo> condition, Func<MyFileInfo, long> sorting)
+        {
+            var files = new List<MyFileInfo>();
+
+            foreach (var fi in new DirectoryInfo(path).GetFiles())
+            {
+                if (condition(fi))
+                {
+                    files.Add(new MyFileInfo() { Name = fi.Name, Length = fi.Length, CreationTime = fi.CreationTime });
+                }
+            }
+
+            DisplayResults("Filter using function parameter: " + path, files.OrderBy(sorting));
+
+        }
+        #endregion
 
         #region Lambda expressions for sorting
 
@@ -201,11 +368,11 @@ namespace LanguageExtensions
                 Console.WriteLine("C:                                       L: Lambda Expressions");
                 Console.WriteLine("D:                                       M: Lambda expressions for sorting");
                 Console.WriteLine("E: Object initialization                 N: Lambda parameters for sorting");
-                Console.WriteLine("F: Object initialization (constructor)   O: Static Method");
-                Console.WriteLine("G: Object initialization (revisited)     P: Extension method");
+                Console.WriteLine("F: Object initialization (constructor)   O: ");
+                Console.WriteLine("G: Object initialization (revisited)     P: Extension Method");
                 Console.WriteLine("H: Add specific files                    Q: Query methods");
                 Console.WriteLine("I: Test Delegates                        R: Enumerable methods");
-                Console.WriteLine("                                         S: Anonymous type");
+                Console.WriteLine("                                         S: Anonymous Type");
 
                 Console.WriteLine("\nEnter an Option (Press . to exit):");
 
@@ -255,10 +422,21 @@ namespace LanguageExtensions
                         LambdaExpressionsForSorting();
                         break;
                     case 'N':
-                        
+                        LambdaParametersForSorting();
                         break;
                     case 'O':
-                        
+                        break;
+                    case 'P':
+                        ExtensionMethod();
+                        break;
+                    case 'Q':
+                        QeryMethods();
+                        break;
+                    case 'R':
+                        EnumerableMethods();
+                        break;
+                    case 'S':
+                        AnonymousType();
                         break;
                     default:
                         return;
